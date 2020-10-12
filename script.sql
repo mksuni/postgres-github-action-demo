2b2f063c3a9f8aa03811404fb011ee403de7cab8
Sunitha Muthukrishna <sumuth@microsoft.com>
2020-10-12 15:16:40 -0700

    Update script.sql

--- script.sql
+++ script.sql
@@ -11,10 +11,15 @@ ALTER SEQUENCE table1_seq RESTART WITH 22;
 
 CREATE TABLE table2 (id int NOT NULL,  fullname  varchar(200), comment varchar(200));
 
+                                                                               
 INSERT INTO table1 (val,txt) VALUES(200, 'test1');
+                                                                               
+INSERT INTO table1 (val,txt) VALUES(300, 'test2');
 
-INSERT INTO table2 (id, val,txt) VALUES (5, 200,'test2');
+INSERT INTO table1 (val,txt) VALUES(400, 'test3');
 
-INSERT INTO table1 (val,txt) VALUES(200, 'test1');
-
-INSERT INTO table2 (id, val,txt) VALUES (5, 200,'test2');
+INSERT INTO table2 (id, fullname,comment) VALUES (1, 'name1','test1');
+                                                                               
+INSERT INTO table2  (id, fullname,comment)VALUES (2, 'name3','test3');
+                                                                               
+INSERT INTO table2  (id, fullname,comment)VALUES (3, 'name2','test2');
